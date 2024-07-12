e03fbb36b127133f5641a2869d0436329a5512cb
minhvo99 <pikachu43dn@gmail.com>
2024-07-12 16:38:30 +0700

    create user model

--- src/controllers/Tour.controller.ts
+++ src/controllers/Tour.controller.ts
@@ -1,19 +1,32 @@
 import { NextFunction, Request, Response } from "express";
-import Tour from "../model/tour.model";
+import Tour from "../model/Tour.model";
 
 export const getAllTour = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const tours = await Tour.find()
         res.status(200).json({
             message: 'Get all tour is successfuly!',
-            data: {
-                tours
-            }
+            data: tours
         })
     } catch (error) {
         res.json({
             message: `An error occurred: ${error}`
         })
         next()
+    }
+}
+
+export const createTour = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const newTour = await Tour.create(req.body);
+        res.status(201).json({
+            message: 'Create a new tour successfully!',
+            data: newTour
+        })
+    } catch (error) {
+        res.json({
+            message: `An error occurred: ${error}`
+        })
+        next() 
     }
 }
