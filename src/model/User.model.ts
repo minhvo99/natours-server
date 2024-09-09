920d6da7d0ca52aeaa4f7934f3be04dda06644f5
huynht <huynht@solazu.com>
2024-09-09 08:35:05 +0700

    add validator lib, user model

--- src/model/User.model.ts
+++ src/model/User.model.ts
@@ -5,6 +5,12 @@ const userSchema = new Schema({
       type: String,
       required: [true, 'A User must have a name'],
    },
+   email: {
+      type: String,
+      required: [true, 'Please provice a valid email'],
+      unique: true,
+      lowercase: true,
+   }
 });
 const User = mongoose.model('User', userSchema);
 
