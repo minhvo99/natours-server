c53b30d1e7e58ddf47c3cb0bba082563d4e9f960
huynht <huynht@solazu.com>
2024-10-26 17:16:37 +0700

    implement method update my profile

--- src/controllers/User.controller.ts
+++ src/controllers/User.controller.ts
@@ -3,6 +3,16 @@ import { Request, Response, NextFunction } from 'express';
 import logger from '../logger/winston';
 import AppError from '../utils/appError';
 
+const filterObj = (obj: { [key: string]: any }, ...allowUpdated: string[]) => {
+   const newObj: { [key: string]: any } = {};
+   Object.keys(obj).forEach((field: string) => {
+      if (!allowUpdated.includes(field)) {
+         newObj[field] = obj[field];
+      }
+   });
+   return newObj;
+};
+
 export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
    try {
       const users = await User.find();
@@ -14,6 +24,33 @@ export const getAllUsers = async (req: Request, res: Response, next: NextFunctio
       logger.error(`Get all user error: ${error}`);
       next(error);
    }
+};
+
+export const updateMyProfile = async (req: Request, res: Response, next: NextFunction) => {
+   try {
+      //1) Create error if user POST password data
+      if (req.body.password || req.body.passWordConfirm) {
+         return next(
+            new AppError(
+               'This route is not for password updated. Please use /change-password.',
+               400,
+            ),
+         );
+      }
+
+      //2) Update user document
+
+      const userUpdate = filterObj(req.body, 'role');
+      const user = await User.findByIdAndUpdate((req as any).user.id, userUpdate, {
+         new: true,
+         runValidators: true,
+      });
+
+      res.status(200).json({
+         status: 'success',
+         user,
+      });
+   } catch (error) {}
 };
 
 export const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getAllUsers, deleteUser } from '../controllers/User.controller';
+import { getAllUsers, deleteUser, updateMyProfile } from '../controllers/User.controller';
+import { authorization, restrictTo } from '../controllers/Auth.controller';
 const userRoute = express.Router();
 
-userRoute.get('/', getAllUsers);
-userRoute.delete('/:id', deleteUser);
+userRoute.get('/', authorization, getAllUsers);
+userRoute.delete('/:id', authorization, restrictTo('admin'), deleteUser);
+userRoute.patch('/update-my-profile', authorization, updateMyProfile)
 
 export default userRoute;
